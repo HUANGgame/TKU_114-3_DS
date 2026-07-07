8d1042383c2dd00420e747cf8d2cd8fc9f66e9dd
goldfish <81166150+HUANGgame@users.noreply.github.com>
2026-07-07 09:28:06 +0800

    Remove extra 0707 practice files

--- 0707/AgeCheck.java
+++
@@ -1,9 +0,0 @@
-public class AgeCheck {
-    public static void main(String[] args) {
-        int age = 20;
-        boolean adult = age >= 18;
-
-        System.out.println("Age: " + age);
-        System.out.println("Adult: " + adult);
-    }
-}
--- 0707/HighScoreCheck.java
+++
@@ -1,9 +0,0 @@
-public class HighScoreCheck {
-    public static void main(String[] args) {
-        int score = 95;
-
-        if (score >= 90) {
-            System.out.println("Excellent");
-        }
-    }
-}
