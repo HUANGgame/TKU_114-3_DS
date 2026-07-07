0708a265cf1b0aacf03dc4b451767b44de82421c
goldfish <81166150+HUANGgame@users.noreply.github.com>
2026-07-07 09:26:55 +0800

    Add 0707 condition and loop practice

---
+++ 0707/WhileInputDemo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class WhileInputDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("請輸入整數：");
+        int number = sc.nextInt();
+
+        while (number != 0) {
+            System.out.println("Number: " + number);
+            System.out.print("請輸入整數：");
+            number = sc.nextInt();
+        }
+
+        sc.close();
+    }
+}
